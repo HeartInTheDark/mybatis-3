cbb002348a216e5ea81af34601b96a095921e293
zhangweinan <1977559097@qq.com>
2023-01-05 17:36:22 +0800

    添加BaseWrapper注释完成

--- src/main/java/org/apache/ibatis/reflection/wrapper/BaseWrapper.java
+++ src/main/java/org/apache/ibatis/reflection/wrapper/BaseWrapper.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.wrapper;
 
@@ -27,81 +27,83 @@ import org.apache.ibatis.reflection.property.PropertyTokenizer;
  */
 public abstract class BaseWrapper implements ObjectWrapper {
 
-  protected static final Object[] NO_ARGUMENTS = new Object[0];
-  protected final MetaObject metaObject;
+    protected static final Object[] NO_ARGUMENTS = new Object[0];
+    protected final MetaObject metaObject;
 
-  protected BaseWrapper(MetaObject metaObject) {
-    this.metaObject = metaObject;
-  }
-
-  protected Object resolveCollection(PropertyTokenizer prop, Object object) {
-    if ("".equals(prop.getName())) {
-      return object;
-    } else {
-      return metaObject.getValue(prop.getName());
+    protected BaseWrapper(MetaObject metaObject) {
+        this.metaObject = metaObject;
     }
-  }
 
-  protected Object getCollectionValue(PropertyTokenizer prop, Object collection) {
-    if (collection instanceof Map) {
-      return ((Map) collection).get(prop.getIndex());
-    } else {
-      int i = Integer.parseInt(prop.getIndex());
-      if (collection instanceof List) {
-        return ((List) collection).get(i);
-      } else if (collection instanceof Object[]) {
-        return ((Object[]) collection)[i];
-      } else if (collection instanceof char[]) {
-        return ((char[]) collection)[i];
-      } else if (collection instanceof boolean[]) {
-        return ((boolean[]) collection)[i];
-      } else if (collection instanceof byte[]) {
-        return ((byte[]) collection)[i];
-      } else if (collection instanceof double[]) {
-        return ((double[]) collection)[i];
-      } else if (collection instanceof float[]) {
-        return ((float[]) collection)[i];
-      } else if (collection instanceof int[]) {
-        return ((int[]) collection)[i];
-      } else if (collection instanceof long[]) {
-        return ((long[]) collection)[i];
-      } else if (collection instanceof short[]) {
-        return ((short[]) collection)[i];
-      } else {
-        throw new ReflectionException("The '" + prop.getName() + "' property of " + collection + " is not a List or Array.");
-      }
+    protected Object resolveCollection(PropertyTokenizer prop, Object object) {
+        if ("".equals(prop.getName())) {
+            return object;
+        } else {
+            return metaObject.getValue(prop.getName());//解析属性表达式并获取指定的属性
+        }
     }
-  }
 
-  protected void setCollectionValue(PropertyTokenizer prop, Object collection, Object value) {
-    if (collection instanceof Map) {
-      ((Map) collection).put(prop.getIndex(), value);
-    } else {
-      int i = Integer.parseInt(prop.getIndex());
-      if (collection instanceof List) {
-        ((List) collection).set(i, value);
-      } else if (collection instanceof Object[]) {
-        ((Object[]) collection)[i] = value;
-      } else if (collection instanceof char[]) {
-        ((char[]) collection)[i] = (Character) value;
-      } else if (collection instanceof boolean[]) {
-        ((boolean[]) collection)[i] = (Boolean) value;
-      } else if (collection instanceof byte[]) {
-        ((byte[]) collection)[i] = (Byte) value;
-      } else if (collection instanceof double[]) {
-        ((double[]) collection)[i] = (Double) value;
-      } else if (collection instanceof float[]) {
-        ((float[]) collection)[i] = (Float) value;
-      } else if (collection instanceof int[]) {
-        ((int[]) collection)[i] = (Integer) value;
-      } else if (collection instanceof long[]) {
-        ((long[]) collection)[i] = (Long) value;
-      } else if (collection instanceof short[]) {
-        ((short[]) collection)[i] = (Short) value;
-      } else {
-        throw new ReflectionException("The '" + prop.getName() + "' property of " + collection + " is not a List or Array.");
-      }
+    //解析属性表达式的索引信息，然后获取对应项
+    protected Object getCollectionValue(PropertyTokenizer prop, Object collection) {
+        if (collection instanceof Map) {//如果为Map类型，则index为key
+            return ((Map) collection).get(prop.getIndex());
+        } else {
+            int i = Integer.parseInt(prop.getIndex());//如果为其他集合类型，则index为下标
+            if (collection instanceof List) {
+                return ((List) collection).get(i);
+            } else if (collection instanceof Object[]) {
+                return ((Object[]) collection)[i];
+            } else if (collection instanceof char[]) {
+                return ((char[]) collection)[i];
+            } else if (collection instanceof boolean[]) {
+                return ((boolean[]) collection)[i];
+            } else if (collection instanceof byte[]) {
+                return ((byte[]) collection)[i];
+            } else if (collection instanceof double[]) {
+                return ((double[]) collection)[i];
+            } else if (collection instanceof float[]) {
+                return ((float[]) collection)[i];
+            } else if (collection instanceof int[]) {
+                return ((int[]) collection)[i];
+            } else if (collection instanceof long[]) {
+                return ((long[]) collection)[i];
+            } else if (collection instanceof short[]) {
+                return ((short[]) collection)[i];
+            } else {
+                throw new ReflectionException("The '" + prop.getName() + "' property of " + collection + " is not a List or Array.");
+            }
+        }
     }
-  }
+
+    //解析属性表达式的索引信息，然后设置对应项
+    protected void setCollectionValue(PropertyTokenizer prop, Object collection, Object value) {
+        if (collection instanceof Map) {
+            ((Map) collection).put(prop.getIndex(), value);
+        } else {
+            int i = Integer.parseInt(prop.getIndex());
+            if (collection instanceof List) {
+                ((List) collection).set(i, value);
+            } else if (collection instanceof Object[]) {
+                ((Object[]) collection)[i] = value;
+            } else if (collection instanceof char[]) {
+                ((char[]) collection)[i] = (Character) value;
+            } else if (collection instanceof boolean[]) {
+                ((boolean[]) collection)[i] = (Boolean) value;
+            } else if (collection instanceof byte[]) {
+                ((byte[]) collection)[i] = (Byte) value;
+            } else if (collection instanceof double[]) {
+                ((double[]) collection)[i] = (Double) value;
+            } else if (collection instanceof float[]) {
+                ((float[]) collection)[i] = (Float) value;
+            } else if (collection instanceof int[]) {
+                ((int[]) collection)[i] = (Integer) value;
+            } else if (collection instanceof long[]) {
+                ((long[]) collection)[i] = (Long) value;
+            } else if (collection instanceof short[]) {
+                ((short[]) collection)[i] = (Short) value;
+            } else {
+                throw new ReflectionException("The '" + prop.getName() + "' property of " + collection + " is not a List or Array.");
+            }
+        }
+    }
 
 }
--- src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapper.java
+++ src/main/java/org/apache/ibatis/reflection/wrapper/ObjectWrapper.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.reflection.wrapper;
 
@@ -23,33 +23,43 @@ import org.apache.ibatis.reflection.property.PropertyTokenizer;
 
 /**
  * @author Clinton Begin
+ * @DES 对对象的包装，抽象了对象的属性信息，定义了一系列查询对象的属性信息的方法，以及更新属性的方法
  */
 public interface ObjectWrapper {
 
-  Object get(PropertyTokenizer prop);
+    //如果ObjectWrapper中封装的是普通Bean对象，则调用相应属性的相应getter方法
+    //如果封装是集合类，则获取指定的key或下标对应的value值
+    Object get(PropertyTokenizer prop);
 
-  void set(PropertyTokenizer prop, Object value);
+    //如果ObjectWrapper中封装的是普通Bean对象，则调用相应属性的相应setter方法
+    //如果封装是集合类，则设置指定的key或下标对应的value值
+    void set(PropertyTokenizer prop, Object value);
 
-  String findProperty(String name, boolean useCamelCaseMapping);
+    //查找属性表达式指定的属性，第二个参数表示是否忽略属性表达式中的下划线
+    String findProperty(String name, boolean useCamelCaseMapping);
 
-  String[] getGetterNames();
+    String[] getGetterNames();//查找可写属性的名称集合
 
-  String[] getSetterNames();
+    String[] getSetterNames();//查找可读属性的名称集合
 
-  Class<?> getSetterType(String name);
+    //解析属性表达式指定的setter方法的参数类型
+    Class<?> getSetterType(String name);
 
-  Class<?> getGetterType(String name);
+    //解析属性表达式指定的getter方法的参数类型
+    Class<?> getGetterType(String name);
 
-  boolean hasSetter(String name);
+    //判断属性表达式是否有getter/setter方法
+    boolean hasSetter(String name);
 
-  boolean hasGetter(String name);
+    boolean hasGetter(String name);
 
-  MetaObject instantiatePropertyValue(String name, PropertyTokenizer prop, ObjectFactory objectFactory);
-  
-  boolean isCollection();
-  
-  void add(Object element);
-  
-  <E> void addAll(List<E> element);
+    //为属性表达式指定的属性创建相应的MetaObject对象
+    MetaObject instantiatePropertyValue(String name, PropertyTokenizer prop, ObjectFactory objectFactory);
+
+    boolean isCollection();//封装对象是否为Collection类型
+
+    void add(Object element);//调用Collection对象的add方法
+
+    <E> void addAll(List<E> element);//调用Collection对象的addAll方法
 
 }
