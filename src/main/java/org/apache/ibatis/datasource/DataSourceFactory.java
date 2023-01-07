b1da6dd9ff358b3f9038d78759a0054bc587a28d
zhangweinan <1977559097@qq.com>
2023-01-07 19:21:14 +0800

    添加 DataSourceFactory 注释

--- src/main/java/org/apache/ibatis/datasource/DataSourceFactory.java
+++ src/main/java/org/apache/ibatis/datasource/DataSourceFactory.java
@@ -23,8 +23,10 @@ import javax.sql.DataSource;
  */
 public interface DataSourceFactory {
 
+  //设置DataSource的相关属性，一般紧跟在初始化完成之后
   void setProperties(Properties props);
 
+  //获取DataSource对象
   DataSource getDataSource();
 
 }
