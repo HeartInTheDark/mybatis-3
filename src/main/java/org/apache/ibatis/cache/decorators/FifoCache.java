e39414a81f79577c4e3bd76da47116c3262de87b
zhangweinan <1977559097@qq.com>
2023-01-08 01:34:19 +0800

    添加 FifoCache 注释

--- src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.cache.decorators;
 
@@ -28,63 +28,65 @@ import org.apache.ibatis.cache.Cache;
  */
 public class FifoCache implements Cache {
 
-  private final Cache delegate;
-  private final Deque<Object> keyList;
-  private int size;
+    private final Cache delegate;// 被装饰的底层Cache对象
 
-  public FifoCache(Cache delegate) {
-    this.delegate = delegate;
-    this.keyList = new LinkedList<Object>();
-    this.size = 1024;
-  }
+    //用于记录key进入缓存后的顺序，使用的是LinkedList类型的集合对象
+    private final Deque<Object> keyList;
+    private int size;//记录缓存项的上限，超过该值，则需要清理最老的缓存项了
 
-  @Override
-  public String getId() {
-    return delegate.getId();
-  }
-
-  @Override
-  public int getSize() {
-    return delegate.getSize();
-  }
-
-  public void setSize(int size) {
-    this.size = size;
-  }
-
-  @Override
-  public void putObject(Object key, Object value) {
-    cycleKeyList(key);
-    delegate.putObject(key, value);
-  }
-
-  @Override
-  public Object getObject(Object key) {
-    return delegate.getObject(key);
-  }
-
-  @Override
-  public Object removeObject(Object key) {
-    return delegate.removeObject(key);
-  }
-
-  @Override
-  public void clear() {
-    delegate.clear();
-    keyList.clear();
-  }
-
-  @Override
-  public ReadWriteLock getReadWriteLock() {
-    return null;
-  }
-
-  private void cycleKeyList(Object key) {
-    keyList.addLast(key);
-    if (keyList.size() > size) {
-      Object oldestKey = keyList.removeFirst();
-      delegate.removeObject(oldestKey);
+    public FifoCache(Cache delegate) {
+        this.delegate = delegate;
+        this.keyList = new LinkedList<Object>();
+        this.size = 1024;
     }
-  }
+
+    @Override
+    public String getId() {
+        return delegate.getId();
+    }
+
+    @Override
+    public int getSize() {
+        return delegate.getSize();
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public void putObject(Object key, Object value) {
+        cycleKeyList(key); //检测并清理缓存
+        delegate.putObject(key, value); //添加缓存项
+    }
+
+    @Override
+    public Object getObject(Object key) {
+        return delegate.getObject(key);
+    }
+
+    @Override
+    public Object removeObject(Object key) {
+        return delegate.removeObject(key);
+    }
+
+    @Override
+    public void clear() {
+        delegate.clear();
+        keyList.clear();
+    }
+
+    @Override
+    public ReadWriteLock getReadWriteLock() {
+        return null;
+    }
+
+    private void cycleKeyList(Object key) {
+        keyList.addLast(key);//记录key
+        if (keyList.size() > size) {//如果达到上限，则清理最老的key项
+            Object oldestKey = keyList.removeFirst();
+            delegate.removeObject(oldestKey);
+        }
+    }
 
 }
