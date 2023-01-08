ecc217a884824e097a2495d61220811b55ea593a
zhangweinan <1977559097@qq.com>
2023-01-08 15:32:25 +0800

    添加 SqlSessionFactoryBuilder 注释

--- src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
+++ src/main/java/org/apache/ibatis/session/SqlSessionFactoryBuilder.java
@@ -46,7 +46,9 @@ public class SqlSessionFactoryBuilder {
 
   public SqlSessionFactory build(Reader reader, String environment, Properties properties) {
     try {
+      //读取配置信息
       XMLConfigBuilder parser = new XMLConfigBuilder(reader, environment, properties);
+      //解析配置文件得到Configuration对象，创建DefaultSqlSessionFactory对象
       return build(parser.parse());
     } catch (Exception e) {
       throw ExceptionFactory.wrapException("Error building SqlSession.", e);
