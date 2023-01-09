37a6171d49021c364d5a3f657dbc2e823f1784e1
zhangweinan <1977559097@qq.com>
2023-01-10 01:21:48 +0800

    添加 SqlNode 注释

--- src/main/java/org/apache/ibatis/scripting/xmltags/SqlNode.java
+++ src/main/java/org/apache/ibatis/scripting/xmltags/SqlNode.java
@@ -19,5 +19,9 @@ package org.apache.ibatis.scripting.xmltags;
  * @author Clinton Begin
  */
 public interface SqlNode {
+
+  //apply()是SqlNode接口中定义的唯一方法，该方法会根据用户传入的实参，参数解析该SqlNode所记录的动态SQL节点，并调用DynamicContext.appendSql()方法将
+  //解析后的SQL片段追加到 DynamicContext.sqlBuilder中保存
+  //当SQL节点下所有的SqlNode完成解析后，我们可以从DynamicContext中获取一条动态生成完整的SQL语句
   boolean apply(DynamicContext context);
 }
