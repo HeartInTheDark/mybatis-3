ffaf0382477b8ec457998a7f0a919c6eaf135a8c
zhangweinan <1977559097@qq.com>
2023-02-02 17:03:39 +0800

    ResultSetWrapper 注释

--- src/main/java/org/apache/ibatis/executor/resultset/ResultSetWrapper.java
+++ src/main/java/org/apache/ibatis/executor/resultset/ResultSetWrapper.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor.resultset;
 
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.ResultMap;
 import org.apache.ibatis.session.Configuration;
@@ -37,154 +38,156 @@ import org.apache.ibatis.type.UnknownTypeHandler;
 
 /**
  * @author Iwao AVE!
+ * 记录了ResultSet的一些元数据，并且提供一系列的操作ResultSet的辅助方法
  */
 public class ResultSetWrapper {
 
-  private final ResultSet resultSet;
-  private final TypeHandlerRegistry typeHandlerRegistry;
-  private final List<String> columnNames = new ArrayList<String>();
-  private final List<String> classNames = new ArrayList<String>();
-  private final List<JdbcType> jdbcTypes = new ArrayList<JdbcType>();
-  private final Map<String, Map<Class<?>, TypeHandler<?>>> typeHandlerMap = new HashMap<String, Map<Class<?>, TypeHandler<?>>>();
-  private final Map<String, List<String>> mappedColumnNamesMap = new HashMap<String, List<String>>();
-  private final Map<String, List<String>> unMappedColumnNamesMap = new HashMap<String, List<String>>();
+    private final ResultSet resultSet; //底层封装了ResultSet对象
+    private final TypeHandlerRegistry typeHandlerRegistry;
+    private final List<String> columnNames = new ArrayList<String>();//记录了ResultSet中每列的列名
+    private final List<String> classNames = new ArrayList<String>();//记录了ResultSet中每列对应的Java类型
+    private final List<JdbcType> jdbcTypes = new ArrayList<JdbcType>();//记录了ResultSet中每列对应的jdbc类型
+    private final Map<String, Map<Class<?>, TypeHandler<?>>> typeHandlerMap = new HashMap<String, Map<Class<?>, TypeHandler<?>>>();//记录了每列对应的TypeHandler对象，key是列名，value是TypeHandler集合
+    private final Map<String, List<String>> mappedColumnNamesMap = new HashMap<String, List<String>>();//记录了被映射的列名，其中key是ResultMap对象的id，value是该ResultMap对象映射的列名集合
+    private final Map<String, List<String>> unMappedColumnNamesMap = new HashMap<String, List<String>>();//记录了未映射的列名，其中key是ResultMap对象的id，value是该ResultMap对象未映射的列名集合
 
-  public ResultSetWrapper(ResultSet rs, Configuration configuration) throws SQLException {
-    super();
-    this.typeHandlerRegistry = configuration.getTypeHandlerRegistry();
-    this.resultSet = rs;
-    final ResultSetMetaData metaData = rs.getMetaData();
-    final int columnCount = metaData.getColumnCount();
-    for (int i = 1; i <= columnCount; i++) {
-      columnNames.add(configuration.isUseColumnLabel() ? metaData.getColumnLabel(i) : metaData.getColumnName(i));
-      jdbcTypes.add(JdbcType.forCode(metaData.getColumnType(i)));
-      classNames.add(metaData.getColumnClassName(i));
-    }
-  }
-
-  public ResultSet getResultSet() {
-    return resultSet;
-  }
-
-  public List<String> getColumnNames() {
-    return this.columnNames;
-  }
-
-  public List<String> getClassNames() {
-    return Collections.unmodifiableList(classNames);
-  }
-
-  public JdbcType getJdbcType(String columnName) {
-    for (int i = 0 ; i < columnNames.size(); i++) {
-      if (columnNames.get(i).equalsIgnoreCase(columnName)) {
-        return jdbcTypes.get(i);
-      }
-    }
-    return null;
-  }
-
-  /**
-   * Gets the type handler to use when reading the result set.
-   * Tries to get from the TypeHandlerRegistry by searching for the property type.
-   * If not found it gets the column JDBC type and tries to get a handler for it.
-   * 
-   * @param propertyType
-   * @param columnName
-   * @return
-   */
-  public TypeHandler<?> getTypeHandler(Class<?> propertyType, String columnName) {
-    TypeHandler<?> handler = null;
-    Map<Class<?>, TypeHandler<?>> columnHandlers = typeHandlerMap.get(columnName);
-    if (columnHandlers == null) {
-      columnHandlers = new HashMap<Class<?>, TypeHandler<?>>();
-      typeHandlerMap.put(columnName, columnHandlers);
-    } else {
-      handler = columnHandlers.get(propertyType);
-    }
-    if (handler == null) {
-      JdbcType jdbcType = getJdbcType(columnName);
-      handler = typeHandlerRegistry.getTypeHandler(propertyType, jdbcType);
-      // Replicate logic of UnknownTypeHandler#resolveTypeHandler
-      // See issue #59 comment 10
-      if (handler == null || handler instanceof UnknownTypeHandler) {
-        final int index = columnNames.indexOf(columnName);
-        final Class<?> javaType = resolveClass(classNames.get(index));
-        if (javaType != null && jdbcType != null) {
-          handler = typeHandlerRegistry.getTypeHandler(javaType, jdbcType);
-        } else if (javaType != null) {
-          handler = typeHandlerRegistry.getTypeHandler(javaType);
-        } else if (jdbcType != null) {
-          handler = typeHandlerRegistry.getTypeHandler(jdbcType);
+    public ResultSetWrapper(ResultSet rs, Configuration configuration) throws SQLException {
+        super();
+        this.typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+        this.resultSet = rs;
+        final ResultSetMetaData metaData = rs.getMetaData();//获取ResultSet元信息
+        final int columnCount = metaData.getColumnCount();//ResultSet中的列数
+        for (int i = 1; i <= columnCount; i++) {
+            //获取列名或是通过"AS"关键字指定的别名
+            columnNames.add(configuration.isUseColumnLabel() ? metaData.getColumnLabel(i) : metaData.getColumnName(i));
+            jdbcTypes.add(JdbcType.forCode(metaData.getColumnType(i)));//该列的jdbc类型
+            classNames.add(metaData.getColumnClassName(i));//该列的Java类型
         }
-      }
-      if (handler == null || handler instanceof UnknownTypeHandler) {
-        handler = new ObjectTypeHandler();
-      }
-      columnHandlers.put(propertyType, handler);
     }
-    return handler;
-  }
 
-  private Class<?> resolveClass(String className) {
-    try {
-      // #699 className could be null
-      if (className != null) {
-        return Resources.classForName(className);
-      }
-    } catch (ClassNotFoundException e) {
-      // ignore
+    public ResultSet getResultSet() {
+        return resultSet;
     }
-    return null;
-  }
 
-  private void loadMappedAndUnmappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
-    List<String> mappedColumnNames = new ArrayList<String>();
-    List<String> unmappedColumnNames = new ArrayList<String>();
-    final String upperColumnPrefix = columnPrefix == null ? null : columnPrefix.toUpperCase(Locale.ENGLISH);
-    final Set<String> mappedColumns = prependPrefixes(resultMap.getMappedColumns(), upperColumnPrefix);
-    for (String columnName : columnNames) {
-      final String upperColumnName = columnName.toUpperCase(Locale.ENGLISH);
-      if (mappedColumns.contains(upperColumnName)) {
-        mappedColumnNames.add(upperColumnName);
-      } else {
-        unmappedColumnNames.add(columnName);
-      }
+    public List<String> getColumnNames() {
+        return this.columnNames;
     }
-    mappedColumnNamesMap.put(getMapKey(resultMap, columnPrefix), mappedColumnNames);
-    unMappedColumnNamesMap.put(getMapKey(resultMap, columnPrefix), unmappedColumnNames);
-  }
 
-  public List<String> getMappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
-    List<String> mappedColumnNames = mappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
-    if (mappedColumnNames == null) {
-      loadMappedAndUnmappedColumnNames(resultMap, columnPrefix);
-      mappedColumnNames = mappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+    public List<String> getClassNames() {
+        return Collections.unmodifiableList(classNames);
     }
-    return mappedColumnNames;
-  }
 
-  public List<String> getUnmappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
-    List<String> unMappedColumnNames = unMappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
-    if (unMappedColumnNames == null) {
-      loadMappedAndUnmappedColumnNames(resultMap, columnPrefix);
-      unMappedColumnNames = unMappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+    public JdbcType getJdbcType(String columnName) {
+        for (int i = 0; i < columnNames.size(); i++) {
+            if (columnNames.get(i).equalsIgnoreCase(columnName)) {
+                return jdbcTypes.get(i);
+            }
+        }
+        return null;
     }
-    return unMappedColumnNames;
-  }
 
-  private String getMapKey(ResultMap resultMap, String columnPrefix) {
-    return resultMap.getId() + ":" + columnPrefix;
-  }
+    /**
+     * Gets the type handler to use when reading the result set.
+     * Tries to get from the TypeHandlerRegistry by searching for the property type.
+     * If not found it gets the column JDBC type and tries to get a handler for it.
+     *
+     * @param propertyType
+     * @param columnName
+     * @return
+     */
+    public TypeHandler<?> getTypeHandler(Class<?> propertyType, String columnName) {
+        TypeHandler<?> handler = null;
+        Map<Class<?>, TypeHandler<?>> columnHandlers = typeHandlerMap.get(columnName);
+        if (columnHandlers == null) {
+            columnHandlers = new HashMap<Class<?>, TypeHandler<?>>();
+            typeHandlerMap.put(columnName, columnHandlers);
+        } else {
+            handler = columnHandlers.get(propertyType);
+        }
+        if (handler == null) {
+            JdbcType jdbcType = getJdbcType(columnName);
+            handler = typeHandlerRegistry.getTypeHandler(propertyType, jdbcType);
+            // Replicate logic of UnknownTypeHandler#resolveTypeHandler
+            // See issue #59 comment 10
+            if (handler == null || handler instanceof UnknownTypeHandler) {
+                final int index = columnNames.indexOf(columnName);
+                final Class<?> javaType = resolveClass(classNames.get(index));
+                if (javaType != null && jdbcType != null) {
+                    handler = typeHandlerRegistry.getTypeHandler(javaType, jdbcType);
+                } else if (javaType != null) {
+                    handler = typeHandlerRegistry.getTypeHandler(javaType);
+                } else if (jdbcType != null) {
+                    handler = typeHandlerRegistry.getTypeHandler(jdbcType);
+                }
+            }
+            if (handler == null || handler instanceof UnknownTypeHandler) {
+                handler = new ObjectTypeHandler();
+            }
+            columnHandlers.put(propertyType, handler);
+        }
+        return handler;
+    }
 
-  private Set<String> prependPrefixes(Set<String> columnNames, String prefix) {
-    if (columnNames == null || columnNames.isEmpty() || prefix == null || prefix.length() == 0) {
-      return columnNames;
+    private Class<?> resolveClass(String className) {
+        try {
+            // #699 className could be null
+            if (className != null) {
+                return Resources.classForName(className);
+            }
+        } catch (ClassNotFoundException e) {
+            // ignore
+        }
+        return null;
     }
-    final Set<String> prefixed = new HashSet<String>();
-    for (String columnName : columnNames) {
-      prefixed.add(prefix + columnName);
+
+    private void loadMappedAndUnmappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
+        List<String> mappedColumnNames = new ArrayList<String>();
+        List<String> unmappedColumnNames = new ArrayList<String>();
+        final String upperColumnPrefix = columnPrefix == null ? null : columnPrefix.toUpperCase(Locale.ENGLISH);
+        final Set<String> mappedColumns = prependPrefixes(resultMap.getMappedColumns(), upperColumnPrefix);
+        for (String columnName : columnNames) {
+            final String upperColumnName = columnName.toUpperCase(Locale.ENGLISH);
+            if (mappedColumns.contains(upperColumnName)) {
+                mappedColumnNames.add(upperColumnName);
+            } else {
+                unmappedColumnNames.add(columnName);
+            }
+        }
+        mappedColumnNamesMap.put(getMapKey(resultMap, columnPrefix), mappedColumnNames);
+        unMappedColumnNamesMap.put(getMapKey(resultMap, columnPrefix), unmappedColumnNames);
     }
-    return prefixed;
-  }
-  
+
+    public List<String> getMappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
+        List<String> mappedColumnNames = mappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+        if (mappedColumnNames == null) {
+            loadMappedAndUnmappedColumnNames(resultMap, columnPrefix);
+            mappedColumnNames = mappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+        }
+        return mappedColumnNames;
+    }
+
+    public List<String> getUnmappedColumnNames(ResultMap resultMap, String columnPrefix) throws SQLException {
+        List<String> unMappedColumnNames = unMappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+        if (unMappedColumnNames == null) {
+            loadMappedAndUnmappedColumnNames(resultMap, columnPrefix);
+            unMappedColumnNames = unMappedColumnNamesMap.get(getMapKey(resultMap, columnPrefix));
+        }
+        return unMappedColumnNames;
+    }
+
+    private String getMapKey(ResultMap resultMap, String columnPrefix) {
+        return resultMap.getId() + ":" + columnPrefix;
+    }
+
+    private Set<String> prependPrefixes(Set<String> columnNames, String prefix) {
+        if (columnNames == null || columnNames.isEmpty() || prefix == null || prefix.length() == 0) {
+            return columnNames;
+        }
+        final Set<String> prefixed = new HashSet<String>();
+        for (String columnName : columnNames) {
+            prefixed.add(prefix + columnName);
+        }
+        return prefixed;
+    }
+
 }
