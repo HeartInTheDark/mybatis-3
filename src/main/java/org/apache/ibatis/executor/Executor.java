57c033e9e986fe61f9e3a7c245222c89df8ba246
zhangweinan <1977559097@qq.com>
2023-02-02 18:27:59 +0800

    Executor 注释

--- src/main/java/org/apache/ibatis/executor/Executor.java
+++ src/main/java/org/apache/ibatis/executor/Executor.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2015 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor;
 
@@ -29,39 +29,43 @@ import org.apache.ibatis.transaction.Transaction;
 
 /**
  * @author Clinton Begin
+ * 定义了数据库操作的基本方法
  */
 public interface Executor {
 
-  ResultHandler NO_RESULT_HANDLER = null;
+    ResultHandler NO_RESULT_HANDLER = null;
 
-  int update(MappedStatement ms, Object parameter) throws SQLException;
+    int update(MappedStatement ms, Object parameter) throws SQLException;//执行update、insert、delete三种类型的SQL语句
 
-  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, CacheKey cacheKey, BoundSql boundSql) throws SQLException;
+    //执行select类型的SQL语句，返回值分为结果对象列表或游标对象
+    <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, CacheKey cacheKey, BoundSql boundSql) throws SQLException;
 
-  <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException;
+    <E> List<E> query(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler) throws SQLException;
 
-  <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException;
+    <E> Cursor<E> queryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds) throws SQLException;
 
-  List<BatchResult> flushStatements() throws SQLException;
+    List<BatchResult> flushStatements() throws SQLException;//批量执行SQL语句
 
-  void commit(boolean required) throws SQLException;
+    void commit(boolean required) throws SQLException; //提交事务
 
-  void rollback(boolean required) throws SQLException;
+    void rollback(boolean required) throws SQLException;//回滚事务
 
-  CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql);
+    //创建缓冲中用到的CacheKey对象
+    CacheKey createCacheKey(MappedStatement ms, Object parameterObject, RowBounds rowBounds, BoundSql boundSql);
 
-  boolean isCached(MappedStatement ms, CacheKey key);
+    boolean isCached(MappedStatement ms, CacheKey key);//根据CacheKey对象查找缓存
 
-  void clearLocalCache();
+    void clearLocalCache();//清空一级缓存
 
-  void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType);
+    //延时加载一级缓存中的数据
+    void deferLoad(MappedStatement ms, MetaObject resultObject, String property, CacheKey key, Class<?> targetType);
 
-  Transaction getTransaction();
+    Transaction getTransaction();//获取事务对象
 
-  void close(boolean forceRollback);
+    void close(boolean forceRollback);//关闭Executor对象
 
-  boolean isClosed();
+    boolean isClosed(); //检测Executor对象是否关闭
 
-  void setExecutorWrapper(Executor executor);
+    void setExecutorWrapper(Executor executor);
 
 }
