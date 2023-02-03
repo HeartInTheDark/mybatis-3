566a3edbb71134dd7500a670152218b5cea873f6
zhangweinan <1977559097@qq.com>
2023-02-03 13:01:12 +0800

    RoutingStatementHandler 注释

--- src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor.statement;
 
@@ -31,66 +31,75 @@ import org.apache.ibatis.session.RowBounds;
 
 /**
  * @author Clinton Begin
+ *
+ * 对于RoutingStatementHandler在整个StatementHandler接口层次中的扮演角色，有人觉得它是一个装饰器，
+ * 但它并没有提供功能上的扩展；有人觉得这里使用了策略模式；还有人认为它是一个静态代理类。个人倾向于策略模式的观点
+ *
+ * RoutingStatementHandler会根据MappedStatement中指定的statementType字段，创建对应的StatementHandler接口实现
  */
 public class RoutingStatementHandler implements StatementHandler {
 
-  private final StatementHandler delegate;
+    private final StatementHandler delegate;//底层封装的真正的StatementHandler对象
 
-  public RoutingStatementHandler(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
+    public RoutingStatementHandler(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
 
-    switch (ms.getStatementType()) {
-      case STATEMENT:
-        delegate = new SimpleStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      case PREPARED:
-        delegate = new PreparedStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      case CALLABLE:
-        delegate = new CallableStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
-        break;
-      default:
-        throw new ExecutorException("Unknown statement type: " + ms.getStatementType());
+        //RoutingStatementHandler的主要功能就是根据MappedStatement的配置，生成一个
+        //对应的StatementHandler对象，并设置到delegate字段中
+        switch (ms.getStatementType()) {
+            case STATEMENT:
+                delegate = new SimpleStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+                break;
+            case PREPARED:
+                delegate = new PreparedStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+                break;
+            case CALLABLE:
+                delegate = new CallableStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
+                break;
+            default:
+                throw new ExecutorException("Unknown statement type: " + ms.getStatementType());
+        }
+
     }
 
-  }
+    //RoutingStatementHandler中的所有方法，都是通过调用delegate对象的对应方法实现的
 
-  @Override
-  public Statement prepare(Connection connection, Integer transactionTimeout) throws SQLException {
-    return delegate.prepare(connection, transactionTimeout);
-  }
+    @Override
+    public Statement prepare(Connection connection, Integer transactionTimeout) throws SQLException {
+        return delegate.prepare(connection, transactionTimeout);
+    }
 
-  @Override
-  public void parameterize(Statement statement) throws SQLException {
-    delegate.parameterize(statement);
-  }
+    @Override
+    public void parameterize(Statement statement) throws SQLException {
+        delegate.parameterize(statement);
+    }
 
-  @Override
-  public void batch(Statement statement) throws SQLException {
-    delegate.batch(statement);
-  }
+    @Override
+    public void batch(Statement statement) throws SQLException {
+        delegate.batch(statement);
+    }
 
-  @Override
-  public int update(Statement statement) throws SQLException {
-    return delegate.update(statement);
-  }
+    @Override
+    public int update(Statement statement) throws SQLException {
+        return delegate.update(statement);
+    }
 
-  @Override
-  public <E> List<E> query(Statement statement, ResultHandler resultHandler) throws SQLException {
-    return delegate.<E>query(statement, resultHandler);
-  }
+    @Override
+    public <E> List<E> query(Statement statement, ResultHandler resultHandler) throws SQLException {
+        return delegate.<E>query(statement, resultHandler);
+    }
 
-  @Override
-  public <E> Cursor<E> queryCursor(Statement statement) throws SQLException {
-    return delegate.queryCursor(statement);
-  }
+    @Override
+    public <E> Cursor<E> queryCursor(Statement statement) throws SQLException {
+        return delegate.queryCursor(statement);
+    }
 
-  @Override
-  public BoundSql getBoundSql() {
-    return delegate.getBoundSql();
-  }
+    @Override
+    public BoundSql getBoundSql() {
+        return delegate.getBoundSql();
+    }
 
-  @Override
-  public ParameterHandler getParameterHandler() {
-    return delegate.getParameterHandler();
-  }
+    @Override
+    public ParameterHandler getParameterHandler() {
+        return delegate.getParameterHandler();
+    }
 }
