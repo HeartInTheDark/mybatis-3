121cf90ae7183f4aaf39d21237e09f9570e779e5
zhangweinan <1977559097@qq.com>
2023-02-03 16:08:28 +0800

    SimpleExecutor 注释

--- src/main/java/org/apache/ibatis/executor/SimpleExecutor.java
+++ src/main/java/org/apache/ibatis/executor/SimpleExecutor.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2016 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2016 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor;
 
@@ -33,58 +33,68 @@ import java.util.List;
 
 /**
  * @author Clinton Begin
+ *
+ * SimpleExecutor继承了BaseExecutor抽象类，它是最简单的Executor接口实现。正如前面所说，Executor使用了模板方法模式，
+ * 一级缓存等固定不变的操作都封装到了BaseExecutor中，在SimpleExecutor中就不必再关心一级缓存等操作，只需要专注实现4个
+ * 基本方法的实现即可
  */
 public class SimpleExecutor extends BaseExecutor {
 
-  public SimpleExecutor(Configuration configuration, Transaction transaction) {
-    super(configuration, transaction);
-  }
-
-  @Override
-  public int doUpdate(MappedStatement ms, Object parameter) throws SQLException {
-    Statement stmt = null;
-    try {
-      Configuration configuration = ms.getConfiguration();
-      StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, RowBounds.DEFAULT, null, null);
-      stmt = prepareStatement(handler, ms.getStatementLog());
-      return handler.update(stmt);
-    } finally {
-      closeStatement(stmt);
+    public SimpleExecutor(Configuration configuration, Transaction transaction) {
+        super(configuration, transaction);
     }
-  }
 
-  @Override
-  public <E> List<E> doQuery(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
-    Statement stmt = null;
-    try {
-      Configuration configuration = ms.getConfiguration();
-      StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, resultHandler, boundSql);
-      stmt = prepareStatement(handler, ms.getStatementLog());
-      return handler.<E>query(stmt, resultHandler);
-    } finally {
-      closeStatement(stmt);
+    @Override
+    public int doUpdate(MappedStatement ms, Object parameter) throws SQLException {
+        Statement stmt = null;
+        try {
+            Configuration configuration = ms.getConfiguration();
+            StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, RowBounds.DEFAULT, null, null);
+            stmt = prepareStatement(handler, ms.getStatementLog());
+            return handler.update(stmt);
+        } finally {
+            closeStatement(stmt);
+        }
     }
-  }
 
-  @Override
-  protected <E> Cursor<E> doQueryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds, BoundSql boundSql) throws SQLException {
-    Configuration configuration = ms.getConfiguration();
-    StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, null, boundSql);
-    Statement stmt = prepareStatement(handler, ms.getStatementLog());
-    return handler.<E>queryCursor(stmt);
-  }
+    @Override
+    public <E> List<E> doQuery(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
+        Statement stmt = null;
+        try {
+            Configuration configuration = ms.getConfiguration();
+            //创建StatementHandler对象，实际返回的是RoutingStatementHandler对象，
+            //其中根据MappedStatement,statementType选择具体的StatementHandler实现
+            StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, resultHandler, boundSql);
 
-  @Override
-  public List<BatchResult> doFlushStatements(boolean isRollback) throws SQLException {
-    return Collections.emptyList();
-  }
+            //完成Statement的创建和初始化，该方法首先会调用StatementHandler.prepare()方法创建
+            //Statement对象，然后调用StatementHandler,parameterize()方法处理占位符
+            stmt = prepareStatement(handler, ms.getStatementLog());
+            //调用StatementHandler,query()方法，执行SQL语句，并通过ResultSetHandler完成结果集的映射
+            return handler.<E>query(stmt, resultHandler);
+        } finally {
+            closeStatement(stmt);//关闭Statement对象
+        }
+    }
 
-  private Statement prepareStatement(StatementHandler handler, Log statementLog) throws SQLException {
-    Statement stmt;
-    Connection connection = getConnection(statementLog);
-    stmt = handler.prepare(connection, transaction.getTimeout());
-    handler.parameterize(stmt);
-    return stmt;
-  }
+    @Override
+    protected <E> Cursor<E> doQueryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds, BoundSql boundSql) throws SQLException {
+        Configuration configuration = ms.getConfiguration();
+        StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, null, boundSql);
+        Statement stmt = prepareStatement(handler, ms.getStatementLog());
+        return handler.<E>queryCursor(stmt);
+    }
+
+    @Override
+    public List<BatchResult> doFlushStatements(boolean isRollback) throws SQLException {
+        return Collections.emptyList();
+    }
+
+    private Statement prepareStatement(StatementHandler handler, Log statementLog) throws SQLException {
+        Statement stmt;
+        Connection connection = getConnection(statementLog);
+        stmt = handler.prepare(connection, transaction.getTimeout());//创建Statement对象
+        handler.parameterize(stmt);//对象占位符
+        return stmt;
+    }
 
 }
