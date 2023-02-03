8fe8aa17f5df20be029bc474cc0ce197f1067752
zhangweinan <1977559097@qq.com>
2023-02-03 12:01:00 +0800

    Jdbc3KeyGenerator 注释

--- src/main/java/org/apache/ibatis/executor/keygen/Jdbc3KeyGenerator.java
+++ src/main/java/org/apache/ibatis/executor/keygen/Jdbc3KeyGenerator.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2009-2017 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2017 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.executor.keygen;
 
@@ -38,107 +38,126 @@ import org.apache.ibatis.type.TypeHandlerRegistry;
 /**
  * @author Clinton Begin
  * @author Kazuki Shimizu
+ *
+ * 用于取回数据库生成的自增id，它对应于mybatis-config.xml配置文件中的useGeneratedKeys全局配置，
+ * 以及映射配置文件中SQL节点（＜insert＞节点）的useGeneratedKeys属性
+ *
+ * Jdbc3KeyGenerator.processBefore（）方法是空实现，只实现了processAfter（）方法，
+ * 该方法会调用Jdbc3KeyGenerator.processBatch（）方法将SQL语句执行后生成的主键记录到用户传递的实参中。
+ * 一般情况下，对于单行插入操作，传入的实参是一个JavaBean对象或是Map对象，则该对象对应一次插入操作的内容；
+ * 对于多行插入，传入的实参可以是对象或Map对象的数组或集合，集合中每一个元素都对应一次插入操作
  */
 public class Jdbc3KeyGenerator implements KeyGenerator {
 
-  /**
-   * A shared instance.
-   * @since 3.4.3
-   */
-  public static final Jdbc3KeyGenerator INSTANCE = new Jdbc3KeyGenerator();
+    /**
+     * A shared instance.
+     * @since 3.4.3
+     */
+    public static final Jdbc3KeyGenerator INSTANCE = new Jdbc3KeyGenerator();
 
-  @Override
-  public void processBefore(Executor executor, MappedStatement ms, Statement stmt, Object parameter) {
-    // do nothing
-  }
+    @Override
+    public void processBefore(Executor executor, MappedStatement ms, Statement stmt, Object parameter) {
+        // do nothing
+    }
 
-  @Override
-  public void processAfter(Executor executor, MappedStatement ms, Statement stmt, Object parameter) {
-    processBatch(ms, stmt, getParameters(parameter));
-  }
+    @Override
+    public void processAfter(Executor executor, MappedStatement ms, Statement stmt, Object parameter) {
+        //将用户传入的实参parameter封装成集合类型，然后传入processBatch()方法中处理
+        processBatch(ms, stmt, getParameters(parameter));
+    }
 
-  public void processBatch(MappedStatement ms, Statement stmt, Collection<Object> parameters) {
-    ResultSet rs = null;
-    try {
-      rs = stmt.getGeneratedKeys();
-      final Configuration configuration = ms.getConfiguration();
-      final TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
-      final String[] keyProperties = ms.getKeyProperties();
-      final ResultSetMetaData rsmd = rs.getMetaData();
-      TypeHandler<?>[] typeHandlers = null;
-      if (keyProperties != null && rsmd.getColumnCount() >= keyProperties.length) {
-        for (Object parameter : parameters) {
-          // there should be one row for each statement (also one for each parameter)
-          if (!rs.next()) {
-            break;
-          }
-          final MetaObject metaParam = configuration.newMetaObject(parameter);
-          if (typeHandlers == null) {
-            typeHandlers = getTypeHandlers(typeHandlerRegistry, metaParam, keyProperties, rsmd);
-          }
-          populateKeys(rs, metaParam, keyProperties, typeHandlers);
-        }
-      }
-    } catch (Exception e) {
-      throw new ExecutorException("Error getting generated key or setting result to parameter object. Cause: " + e, e);
-    } finally {
-      if (rs != null) {
+    //processBatch（）方法会遍历数据库生成的主键结果集，并设置到parameters集合对应元素的属性中
+    public void processBatch(MappedStatement ms, Statement stmt, Collection<Object> parameters) {
+        ResultSet rs = null;
         try {
-          rs.close();
+            //获取数据库自动生成的主键，如果没有生成主键，则返回结果集为空
+            rs = stmt.getGeneratedKeys();
+            final Configuration configuration = ms.getConfiguration();
+            final TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            //获得keyProperties属性指定的属性名称，它表示主键对应的属性名称
+            final String[] keyProperties = ms.getKeyProperties();
+            ///获取ResultSet的元数据信息
+            final ResultSetMetaData rsmd = rs.getMetaData();
+            TypeHandler<?>[] typeHandlers = null;
+            //检测数据库生成的主键的列数与keyProperties属性指定的列数是否匹配
+            if (keyProperties != null && rsmd.getColumnCount() >= keyProperties.length) {
+                for (Object parameter : parameters) {
+                    // there should be one row for each statement (also one for each parameter)
+                    if (!rs.next()) {//parameters中有多少元素，就对应生成多少个主键
+                        break;
+                    }
+                    //为用户传入的实参创建相应的MetaObject对象
+                    final MetaObject metaParam = configuration.newMetaObject(parameter);
+                    if (typeHandlers == null) {
+                        //获取对应的TypeHandler对象
+                        typeHandlers = getTypeHandlers(typeHandlerRegistry, metaParam, keyProperties, rsmd);
+                    }
+                    //将生成的主键设置到用户传入的参数的对应位置
+                    populateKeys(rs, metaParam, keyProperties, typeHandlers);
+                }
+            }
         } catch (Exception e) {
-          // ignore
+            throw new ExecutorException("Error getting generated key or setting result to parameter object. Cause: " + e, e);
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                    // ignore
+                }
+            }
         }
-      }
     }
-  }
 
-  private Collection<Object> getParameters(Object parameter) {
-    Collection<Object> parameters = null;
-    if (parameter instanceof Collection) {
-      parameters = (Collection) parameter;
-    } else if (parameter instanceof Map) {
-      Map parameterMap = (Map) parameter;
-      if (parameterMap.containsKey("collection")) {
-        parameters = (Collection) parameterMap.get("collection");
-      } else if (parameterMap.containsKey("list")) {
-        parameters = (List) parameterMap.get("list");
-      } else if (parameterMap.containsKey("array")) {
-        parameters = Arrays.asList((Object[]) parameterMap.get("array"));
-      }
-    }
-    if (parameters == null) {
-      parameters = new ArrayList<Object>();
-      parameters.add(parameter);
-    }
-    return parameters;
-  }
-
-  private TypeHandler<?>[] getTypeHandlers(TypeHandlerRegistry typeHandlerRegistry, MetaObject metaParam, String[] keyProperties, ResultSetMetaData rsmd) throws SQLException {
-    TypeHandler<?>[] typeHandlers = new TypeHandler<?>[keyProperties.length];
-    for (int i = 0; i < keyProperties.length; i++) {
-      if (metaParam.hasSetter(keyProperties[i])) {
-        TypeHandler<?> th;
-        try {
-          Class<?> keyPropertyType = metaParam.getSetterType(keyProperties[i]);
-          th = typeHandlerRegistry.getTypeHandler(keyPropertyType, JdbcType.forCode(rsmd.getColumnType(i + 1)));
-        } catch (BindingException e) {
-          th = null;
+    //将用户传入的实参转换成Collection类型对象
+    private Collection<Object> getParameters(Object parameter) {
+        Collection<Object> parameters = null;
+        if (parameter instanceof Collection) { //参数类型为Collection类型
+            parameters = (Collection) parameter;
+        } else if (parameter instanceof Map) {
+            //参数为Map类型 获取其中指定的key
+            Map parameterMap = (Map) parameter;
+            if (parameterMap.containsKey("collection")) {
+                parameters = (Collection) parameterMap.get("collection");
+            } else if (parameterMap.containsKey("list")) {
+                parameters = (List) parameterMap.get("list");
+            } else if (parameterMap.containsKey("array")) {
+                parameters = Arrays.asList((Object[]) parameterMap.get("array"));
+            }
         }
-        typeHandlers[i] = th;
-      }
+        if (parameters == null) {//参数为普通对象或不包含上述key的Map集合，则创建ArrayList
+            parameters = new ArrayList<Object>();
+            parameters.add(parameter);
+        }
+        return parameters;
     }
-    return typeHandlers;
-  }
 
-  private void populateKeys(ResultSet rs, MetaObject metaParam, String[] keyProperties, TypeHandler<?>[] typeHandlers) throws SQLException {
-    for (int i = 0; i < keyProperties.length; i++) {
-      String property = keyProperties[i];
-      TypeHandler<?> th = typeHandlers[i];
-      if (th != null) {
-        Object value = th.getResult(rs, i + 1);
-        metaParam.setValue(property, value);
-      }
+    private TypeHandler<?>[] getTypeHandlers(TypeHandlerRegistry typeHandlerRegistry, MetaObject metaParam, String[] keyProperties, ResultSetMetaData rsmd) throws SQLException {
+        TypeHandler<?>[] typeHandlers = new TypeHandler<?>[keyProperties.length];
+        for (int i = 0; i < keyProperties.length; i++) {
+            if (metaParam.hasSetter(keyProperties[i])) {
+                TypeHandler<?> th;
+                try {
+                    Class<?> keyPropertyType = metaParam.getSetterType(keyProperties[i]);
+                    th = typeHandlerRegistry.getTypeHandler(keyPropertyType, JdbcType.forCode(rsmd.getColumnType(i + 1)));
+                } catch (BindingException e) {
+                    th = null;
+                }
+                typeHandlers[i] = th;
+            }
+        }
+        return typeHandlers;
     }
-  }
+
+    private void populateKeys(ResultSet rs, MetaObject metaParam, String[] keyProperties, TypeHandler<?>[] typeHandlers) throws SQLException {
+        for (int i = 0; i < keyProperties.length; i++) {
+            String property = keyProperties[i];
+            TypeHandler<?> th = typeHandlers[i];
+            if (th != null) {
+                Object value = th.getResult(rs, i + 1);
+                metaParam.setValue(property, value);
+            }
+        }
+    }
 
 }
