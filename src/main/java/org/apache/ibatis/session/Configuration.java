ff79fc25269a81de5e01c0e069f2904187737219
zhangweinan <1977559097@qq.com>
2023-02-03 23:11:41 +0800

    Configuration 注释

--- src/main/java/org/apache/ibatis/session/Configuration.java
+++ src/main/java/org/apache/ibatis/session/Configuration.java
@@ -568,6 +568,7 @@ public class Configuration {
     executorType = executorType == null ? defaultExecutorType : executorType;
     executorType = executorType == null ? ExecutorType.SIMPLE : executorType;
     Executor executor;
+    //根据参数，选择合适的Executor实现
     if (ExecutorType.BATCH == executorType) {
       executor = new BatchExecutor(this, transaction);
     } else if (ExecutorType.REUSE == executorType) {
@@ -575,9 +576,10 @@ public class Configuration {
     } else {
       executor = new SimpleExecutor(this, transaction);
     }
-    if (cacheEnabled) {
+    if (cacheEnabled) {//根据配置决定是否开启二级缓存的功能
       executor = new CachingExecutor(executor);
     }
+    //通过InterceptorChain.pluginAll()方法创建Executor的代理对象
     executor = (Executor) interceptorChain.pluginAll(executor);
     return executor;
   }
